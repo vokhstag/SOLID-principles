ce8ee3c6af0aad3c444f09adbba5a8a5ad4afce9
Yunus <Vokhstag@gmail.com>
2020-01-26 20:59:25 +0300

    Initial Commit

---
+++ SOLID principles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOLID principles
+//
+//  Created by Vokh Stag on 26/01/2020.
+//  Copyright © 2020 Vokh Stag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
